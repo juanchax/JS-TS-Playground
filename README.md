8c56485bf3dfb3038a891b892178da9326a0bbc1
juanchax <juanchax@gmail.com>
2025-03-08 17:02:46 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# JS-TS-Playground
+ Simple template project to create and run JS and TS files, ask for user input (in the terminal window) and use the input as a variable in your code
---
+++ template.ts
@@ -0,0 +1,18 @@
+import * as promptSync from 'prompt-sync'
+
+// create a variable to call as your terminal prompt maker
+const prompt = require('prompt-sync')();
+
+// now let's ask the user for some input
+const name = prompt(`What\'s your name? `);
+
+// and greet them using that input
+
+console.log(`Hey ${name}! Happy you're here!`);
+
+/* Run your Typescript file:
+    1. Open a terminal window in your IDE
+    2. Run 'npx tsx ./myFile.ts' where "myFile.ts" is the name of your file
+
+    Happy coding!
+*/
