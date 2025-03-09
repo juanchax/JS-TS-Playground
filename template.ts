061fcf4fdc3e4b4b981e0cbbf0130494cad9f6dd
juanchax <juanchax@gmail.com>
2025-03-08 21:54:13 -0300

    Update template.ts

--- template.ts
+++ template.ts
@@ -16,5 +16,5 @@ console.log(`Hey ${name}! Happy you're here!`);
     3. Edit or duplicate the provided template.ts file, and add your code. Use the same logic as #2 to execute your files.
     Note: make sure you use the correct path to your file when attempting to execute in the terminal window: if your files are contained in /src but your terminal is open at the root of your project.. see where I'm going?
 
-    Happy coding!
+    Happy coding! test 
 */
