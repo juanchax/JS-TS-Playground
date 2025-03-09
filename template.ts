cb6a1c30ea115210aaa13705c2ecc92714c79e54
juanchax <juanchax@gmail.com>
2025-03-08 21:47:17 -0300

    Update template.ts

--- template.ts
+++ template.ts
@@ -14,7 +14,7 @@ console.log(`Hey ${name}! Happy you're here!`);
     1. Open a terminal window in your IDE
     2. Run 'npx tsx ./src/template.ts' to  run the provided template
     3. Edit or duplicate the provided template.ts file, and add your code. Use the same logic as #2 to execute your files.
-    Note: make sure you use the correct path to your file when attempting to execute in the terminal window: if your files are contained in /src but your terminal is open at the root of your project.. see where I'm going? 
+    Note: make sure you use the correct path to your file when attempting to execute in the terminal window: if your files are contained in /src but your terminal is open at the root of your project.. see where I'm going?
 
-    Happy coding!
+    Happy coding! test
 */
